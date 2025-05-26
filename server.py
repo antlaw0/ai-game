06cacc09871bb4d9f8f08435c27efa8215143746
antlaw0 <antlaw0@gmail.com>
2025-05-26 11:19:23 -0500

    Auto-commit on 26-Mon-05 at 11:19

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, make_response
 import jwt
 import os
 SECRET_KEY=os.getenv('SECRET_KEY')
